f517f8e9b14d53abd0058c66aa186279c2b77059
xcapde <xcapde@hotmail.com>
2022-04-12 00:55:33 +0200

    finalgit add .

--- MenuResponsive.js
+++ MenuResponsive.js
@@ -1,13 +1,11 @@
 
 
 function myFunction() {
-    var x = document.getElementById("menuresponsive"); /*obtenir l'element menú perquè es mostri*/
-    
-    if (x.className === "menulist") { /*si la classe és la indicada, s'afegeix la classe "responsive" al menú*/
-      x.className += "responsive";
-    }
-    else {
-      x.className = "menulist"; /*a més a més, la classe segueix sent la de "menulist"*/
-    }
+  var x = document.getElementById("menuresponsive");
+  if (x.className === "menulist") {
+    x.className += " responsive";
+  } 
+  else {
+    x.className = "menulist";
+  }
 }
-    /*Alterna entre afegir i eliminar la classe "responsive" a la classe del menú quan l'usuari fer click a la icona d'hamburguesa*/
--- Sounds.js
+++ Sounds.js
@@ -1,2 +1,49 @@
-var bombo = new Audio();
-bombo.src ="https://github.com/xcapde/HIT-DE-DOMBOOM/blob/main/Sounds/bombo%20(1).wav"
+///Provar de fer amb --> addEventListener + onclick en el Java i DATA SET en el HTML
+
+var bombo = document.getElementById("bombo"); 
+
+function playBombo() { 
+  bombo.play(); 
+} 
+
+var crash = document.getElementById("crash"); 
+
+function playCrash() { 
+  crash.play(); 
+}
+
+var hit = document.getElementById("hithat"); 
+
+function playHithat() { 
+  hit.play(); 
+}
+
+var redo = document.getElementById("redoblante"); 
+
+function playRedoblante() { 
+  redo.play(); 
+}
+
+var tom1 = document.getElementById("tom1"); 
+
+function playTom1() { 
+  tom1.play(); 
+}
+
+var tom2 = document.getElementById("tom2"); 
+
+function playTom2() { 
+  tom2.play(); 
+}
+
+var tom3 = document.getElementById("tom3"); 
+
+function playTom3() { 
+  tom3.play(); 
+}
+
+var tom4 = document.getElementById("tom4"); 
+
+function playTom4() { 
+  tom4.play(); 
+}
